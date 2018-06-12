82bb6506921e9e414bae55b646c7934841869d21
Matt Kangas <kangas@gmail.com>
2018-06-12 15:51:12 -0400

    pyenv init

--- python.sh
+++ python.sh
@@ -11,4 +11,8 @@ fi
 
 if command -v pyenv 1>/dev/null 2>&1; then
   eval "$(pyenv init -)"
+
+  if [ -d $PYENV_ROOT/plugins/pyenv-virtualenv ]; then
+    eval "$(pyenv virtualenv-init -)"
+  fi
 fi 
